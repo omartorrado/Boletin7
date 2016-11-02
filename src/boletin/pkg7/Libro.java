f41454dd2f84d53e86d5c50a2b9e49898520815c
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-11-02 14:47:01 +0100

    boletin 7

---
+++ src/boletin/pkg7/Libro.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg7;
+
+/**
+ *
+ * @author otorradomiguez
+ */
+public class Libro {
+    //ATTRS
+    private String titulo, autor;
+    private int ejemplares, prestados;
+    //Constructores
+    public Libro(){};
+    
+    public Libro(String titulo, String autor, int ejemplares, int prestados){
+        this.titulo=titulo; this.autor=autor; this.ejemplares=ejemplares;
+        this.prestados=prestados;
+    }
+    //Metodos
+    //Setter-getters
+    public void setTitulo(String titulo){
+        this.titulo=titulo;
+    }
+    public void setAutor(String autor){
+        this.autor=autor;
+    }
+    public void setEjemplares(int ejemplares){
+        this.ejemplares=ejemplares;
+    }
+    public void setPrestados(int prestados){
+        this.prestados=prestados;
+    }
+    public String getTitulo(){
+        return this.titulo;
+    }
+    public String getAutor(){
+        return this.autor;
+    }
+    public int getEjemplares(){
+        return this.ejemplares;
+    }
+    public int getPrestados(){
+        return this.prestados;
+    }
+    //prestamo
+    public boolean Prestamo(){
+        if (this.ejemplares>this.prestados){
+            this.prestados++;
+            return true;
+            }
+        else{
+            return false;
+        }
+    }
+    //devolucion
+    public boolean Devolucion(){
+        if (this.prestados>0){
+        this.prestados--;
+        return true;}
+        else{
+            return false;}
+        }
+    
+}
